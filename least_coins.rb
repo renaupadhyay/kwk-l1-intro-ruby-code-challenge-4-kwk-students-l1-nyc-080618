d9f8e4f3c8a31a6c93b0b9885d4191d66b8b1290
renaupadhyay <renaupadhyay01@gmail.com>
2018-08-10 16:57:50 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,6 +1,21 @@
 #write out your code here
 
+
+
+
 def least_coins(cents)
+  if cents < 25 
+    puts "quarters=0"
+  end
+  if cents >25 && cents <50 
+    puts "quarters = 1"
+  end
+  dimes % 10 
+  nickels % 5
+end
+
+least coins (25)
+  
 
 #Code your answer here!
 
